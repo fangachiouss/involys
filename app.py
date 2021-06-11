5bf4a94d2f5462f40b2f48911d14474ddf5d7101
fangachiouss <fangachioussma@gmail.com>
2021-06-11 05:46:22 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 12 03:57:36 2020
+
+@author: root
+"""
+
+from pycaret.regression import load_model,predict_model
+import streamlit as  st
+import pandas as pd
+import numpy as np
+import datetime as dt
+from sklearn.preprocessing import StandardScaler
+import datetime as dt
+import pickle
+from pickle import *
+import plotly.express as px
+from pycaret.regression import *
+import cv2
+regression =load_model('deployment_28042021')
+#classification =load_model('classification')
+num=dict()
+
+
+def predict(model,input_df):
+    predictions_df=predict_model(estimator=model,data=input_df)
+    predictions=predictions_df
+    return predictions
+def run(): 
+    add_selectbox=st.sidebar.selectbox("how would you to predict",("online","fichier","training","graph"))
+    st.sidebar.success('https://www.pycaret.org')
+    if(add_selectbox=='online'):
+        f1 = open ("column","rb")
+        column=load(f1)
+        type_depense=st.selectbox("Type dépense",np.array(column["Type dépense"]))
+        montant_depense=st.number_input("Montant Dépense")
+        Service_demandeur=st.selectbox("Service demandeur",np.array(column["Service demandeur"]))
+        delai_execution=st.number_input("Délai exécution")
+        Fournisseur=st.selectbox("Fournisseur",np.array(column["Fournisseur"]))
+        date1=st.date_input('Date validation facture') 
+        Montant_facture_HT=st.number_input('Montant facture HT')
+        input_dict={"Type dépense":type_depense,"Montant Dépense":montant_depense,"Service demandeur":Service_demandeur,"Délai exécution":delai_execution,"Fournisseur":Fournisseur,"Date validation facture":date1,"Montant facture HT":Montant_facture_HT}
+        data_predict=pd.DataFrame([input_dict])
+        data_predict['Date validation facture']=pd.to_datetime(data_predict['Date validation facture'])
+        data_predict['Date validation facture']=data_predict['Date validation facture'].map(dt.datetime.toordinal)
+        for col in data_predict.select_dtypes(include=['object']):
+            f = open (col,"rb")
+            num=load(f)
+            data_predict[col]=data_predict[col].map(num)
+        for col in data_predict.select_dtypes(include=['datetime64']):
+            data_predict[col]=data_predict[col].map(dt.datetime.toordinal)            
+        st.write(data_predict)
+        if(st.button("predict")):
+            output=predict(model=regression,input_df=data_predict)
+            st.write(int(output['Label']))
+    if(add_selectbox=='fichier'):  
+        file_upl=st.file_uploader("Upload excel file for predictions" ,type=["xlsx"])
+        if(file_upl is not None):
+            data_predict1=pd.read_excel(file_upl)
+            for col in data.select_dtypes(include=['object']):
+                f = open (col,"rb")
+                num=load(f)
+                data_predict1[col]=data_predict1[col].map(num)
+            for col in data_predict1.select_dtypes(include=['datetime64']):
+                data_predict1[col]=data_predict1[col].map(dt.datetime.toordinal)
+            predictions=predict_model(estimator=regression,data=data_predict1)
+            data['Label']=predictions['Label'].astype(int)
+            st.write(data)
+    if(add_selectbox=='graph'):
+        if(st.button("residual")):
+            st.image("Residuals.png")
+        if(st.button("feature importance")):
+            st.image("Feature Importance.png")
+        if(st.button("feature selection")):
+            st.image("Feature Selection.png")
+        if(st.button("error")):
+            st.image("Prediction Error.png")
+        if(st.button("interpret")):
+            st.image("SHAP summary.png")
+
+
+
+
+
+run()      
